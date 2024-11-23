dff58357e8d65b3e756f6ad6aa7f78ae26957e30
JEIVC <163987022+JEIVC@users.noreply.github.com>
2024-11-23 12:45:58 +1100

    holy carp

--- incremental.js
+++ incremental.js
@@ -1,3 +1,12 @@
+"----------------------------";
+"JEIVC AWESOME TECHNICAL REAL";
+"----------------------------";
+"Since comments break the script (single-line shenanigans), I've resorted to strings.";
+
+
+"The workaround for creating PERSISTENT variables, since normal ones get reset every input.";
+"Allows you to create elements quickly.";
+
 function retrieveElement(varId, defaultValue = "", shouldDisplay = false, elementType = "p", parent = document.body) {
     if (document.querySelector(`#${varId}`)) { 
         val = document.getElementById(varId);
@@ -7,31 +16,48 @@ function retrieveElement(varId, defaultValue = "", shouldDisplay = false, elemen
         val.textContent = defaultValue; 
         if (!shouldDisplay) val.style = "display: none;";
         parent.appendChild(val); 
-    } 
+    }
     return val;
 }
 
-function run(r) {
-    // Initialise elements and variables
+
+"The built-in 'r' variable is a bit limiting, so it's now a function!";
+"Pass nothing to get its value, pass in a string to change it to that parameter.";
+
+let response = document.getElementById("resp");
+let r = (val = null) => {
+    if (val === null) return response.value;
+    else response.value = val;
+};
+
+
+"The bulk of your code. Don't mess with anything outside the function.";
+
+function run() {
+    "Initialise the counter and main game textstuffs.";
     let counter = retrieveElement("gameCounter", "0");
     let gameHeader = retrieveElement("gameHeader", "JEIVC'S AWESOME TECH.FIGGYC INCREMENTAL!!!!", true, "h1");
     let gameText = retrieveElement("gameText", "", true, "pre");
-    let gameInstructions = retrieveElement("gameInst", "", true, "details");
-        let gameInstSummary = retrieveElement("gameInstHeader", "", true, "summary", gameInstructions);
-        let gameInstHeader = retrieveElement("gameInstHeader", "How do I play?", true, "strong", gameInstructions);
-        let gameInstText = retrieveElement("gameInstText", `Idk.\nType "c;" to gain a point!\nCertain upgrades may affect the amount of points you get.`, true, "pre", gameInstructions);
-    let response = document.getElementById("resp");
-    let notes = document.getElementById("notes");
     
-    if (r == "c;") {
+    "Create a game instruction widget.";
+    let gameInstructions = retrieveElement("gameInst", "", true, "details");
+        let gameInstSummary = retrieveElement("gameInstSummary", "", true, "summary", gameInstructions);
+        let gameInstHeader = retrieveElement("gameInstHeader", "How do I play?", true, "strong", gameInstSummary);
+        let gameInstText = retrieveElement("gameInstText", `Idk.\nType "c;" to gain a point!\nCertain upgrades may affect the amount of points you get.`, true, "pre", gameInstructions);
+    
+    "The value doesn't really matter.";
+    let notes = document.getElementById("notes");
+    notes.value = "Hi! I'm Useless. ;']"; 
+
+    if (r() == "c;") {
         counter.textContent = Number(counter.textContent) + 1;
-        response.value = "";
+        r("");
     }
     
-    notes.value = "Hi! I'm Useless. ;']"; 
     gameText.textContent = `You have ${counter.textContent} points!`; 
-    
-    true;
 }
 
-// if(document.querySelector("#techScript"))"Type a letter to run the game! (And don't mess with this note, will ya?)"==document.getElementById("notes").value&&(document.getElementById("resp").value=""),run(r);else{let e=document.createElement("script");fetch("https://raw.githubusercontent.com/JEIVC/figgyc-gaming/refs/heads/main/incremental.js").then(e=>e.text()).then(t=>{e.innerHTML=t,e.id="techScript",document.body.appendChild(e),document.getElementById("notes").value="Type a letter to run the game! (And don't mess with this note, will ya?)"})};true;
+if (document.getElementById("tech").value.includes("Hi! This line of code is important if you want to test this in real-time. I'm self-referential!")) run();
+
+"Flags the response as 'following' the limit. You can change this if you want, this is just as a preference.";
+true;
