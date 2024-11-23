ef9d6ee68a57691cdaa153e29eb164f46c785f24
JEIVC <163987022+JEIVC@users.noreply.github.com>
2024-11-23 12:02:27 +1100

    Update incremental.js

--- incremental.js
+++ incremental.js
@@ -13,25 +13,25 @@ function retrieveElement(varId, defaultValue = "", shouldDisplay = false, elemen
 
 function run(r) {
     // Initialise elements and variables
-    const wordBank = ["ewower", "oil", "technical", "supervoter", "chimera", "minitwow", "response"];
     let counter = retrieveElement("gameCounter", "0");
-    let curWord = retrieveElement("currentWord", "starter");
-    let gameHeader = retrieveElement("gameHeader", "JEIVC'S AWESOME TECH.FIGGYC GAME!!!!", true, "h1");
+    let gameHeader = retrieveElement("gameHeader", "JEIVC'S AWESOME TECH.FIGGYC INCREMENTAL!!!!", true, "h1");
     let gameText = retrieveElement("gameText", "", true, "pre");
     let gameInstructions = retrieveElement("gameInst", "", true, "details");
-    let gameInstHeader = retrieveElement("gameInstHeader", "How do I play??", true, "summary", gameInstructions);
-    let gameInstText = retrieveElement("gameInstText", "Idk", true, "p", gameInstructions);
+        let gameInstSummary = retrieveElement("gameInstHeader", "", true, "summary", gameInstructions);
+        let gameInstHeader = retrieveElement("gameInstHeader", "How do I play?", true, "strong", gameInstructions);
+        let gameInstText = retrieveElement("gameInstText", `Idk.\nType "c;" to gain a point!\nCertain upgrades may affect the amount of points you get.`, true, "pre", gameInstructions);
     let response = document.getElementById("resp");
     let notes = document.getElementById("notes");
     
-    if (r == curWord.textContent) {
+    if (r == "c;") {
         counter.textContent = Number(counter.textContent) + 1;
-        curWord.textContent = wordBank[Math.floor(Math.random() * wordBank.length)];
         response.value = "";
     }
     
     notes.value = "Hi! I'm Useless. ;']"; 
-    gameText.textContent = `You have ${counter.textContent} points!\n\nGet one point by typing the word: ${curWord.textContent}`; 
+    gameText.textContent = `You have ${counter.textContent} points!`; 
     
     true;
 }
+
+// if(document.querySelector("#techScript"))"Type a letter to run the game! (And don't mess with this note, will ya?)"==document.getElementById("notes").value&&(document.getElementById("resp").value=""),run(r);else{let e=document.createElement("script");fetch("https://raw.githubusercontent.com/JEIVC/figgyc-gaming/refs/heads/main/incremental.js").then(e=>e.text()).then(t=>{e.innerHTML=t,e.id="techScript",document.body.appendChild(e),document.getElementById("notes").value="Type a letter to run the game! (And don't mess with this note, will ya?)"})};true;
